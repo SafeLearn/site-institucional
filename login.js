953034abebc1dfd49b4422d4155f8e086af8b547
Diego de Campos <diego.arruda@sptech.school>
2024-03-09 00:19:03 -0300

    Comecando a tela de login da SafeLearn

---
+++ login.js
@@ -0,0 +1,12 @@
+function fazerLogin() {
+    var loginUsuario = usuario.value
+    var loginSenha = senha.value
+
+    if (loginUsuario.length <= 0 || loginSenha.length <= 0) {
+
+    }
+    
+    else {
+
+    }
+}

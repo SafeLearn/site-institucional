b89887a4f03afce0a3c83b15827386dd4f383088
GiovaneCardona <giovane.cardona@sptech.school>
2024-03-16 13:53:50 -0300

    Feat: adding validations to the recuperarsenha screen

---
+++ public/recuperarsenha.js
@@ -0,0 +1,13 @@
+function confirmarEmail() {
+    const mail1 = email.value;
+    const emailConfirmar = cmail.value;
+
+    if (mail1 != emailConfirmar) {
+        alert("Os e-mails estão diferentes!")
+    }else{
+        alert("O código foi enviado!")
+    }
+
+
+
+}

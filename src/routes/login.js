84c92ded0b44cff7c5780afda1686c09e9ecf819
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2024-03-16 01:16:21 -0300

    feat: Tela de login e banco de dados conectados

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,4 +2,15 @@ const { Router } = require("express");
 const router = Router();
 const loginController = require("../controllers/loginController");
 
+router.post("/logar", async (req, res) => {
+    try {
+        const dadosLogin = req.body;
+        const dadosLoginCriado = await loginController.buscar(dadosLogin);
+        res.status(201).json(dadosLoginCriado);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+        console.error(error.message);
+    }
+});
+
 module.exports = router;

f9e37dfb900146904e53222378c9cb8f6afd73b6
GiovaneCardona <giovane.cardona@sptech.school>
2024-03-15 21:12:29 -0300

    Fix: Fixing validations cadastroPessoal

--- public/cadastroPessoal.js
+++ public/cadastroPessoal.js
@@ -1,14 +1,22 @@
 function Continuar() {
-    const loginUsuario = usuario.value
-    const loginSenha = senha.value
-    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-    const nomeCompPessoal = nomeComp.value
-    const telPessoal = telPessoal.value
+    const emailPessoal = emailPessoa.value;
+    const loginSenha = senha.value;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const nomeResp = nomeResponsavel.value;
+    const telResp = telPessoal.value;
+    const selectCargo = cargo.value;
 
-    if (loginUsuario.length <= 0 || loginSenha.length <= 0 || nomeCompPessoal.length <= 0 || telPessoal.length <= 0) {
-        alert("Preencha corretamente o formulário")
-    }else if (!emailRegex.test(emailInstituicao)) {
-        alert("Insira um e-mail válido, contendo @, números e .com ")
-    }
-    window.location.href = "cadastroInstitucional.html";
+    if (emailPessoal.length <= 0 || loginSenha.length <= 0 || nomeResp.length <= 0 || telResp.length <= 0) {
+        alert("Preencha corretamente o formulário");
+    } else if (!emailRegex.test(emailPessoal)) {
+        alert("Insira um e-mail válido, contendo @, números e .com ");
+    } else if (telResp.length <= 8 || telResp.length >= 13) {
+        alert("Insira um número de telefone válido!");
+    }else {
+        window.location.href = "cadastroInstitucional.html";
+    };
+
+
+
+
 }

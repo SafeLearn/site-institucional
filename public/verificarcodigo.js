104d92c248221f5bdf7a44af8d1f27f78113de2c
daniescobarx <daniela.mollo@sptech.school>
2024-03-18 17:03:00 -0300

    ajuste js

---
+++ public/verificarcodigo.js
@@ -0,0 +1,13 @@
+function mostrarOverlay(){
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function ocultarOverlay(){
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function verificarCodigo(){
+    var codigoInserido = document.getElementById('code-input').value;
+    window.location.href = "trocarsenha.html"; 
+    //logica para verificar
+}

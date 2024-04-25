db4dac1d7b33825c85551c17998cfa18ae0bb85b
AnaBeatriz051 <ana.oliveira@spetch.school>
2024-04-25 15:14:38 -0300

    REFACTOR: Trocando gráfico home para de linha

--- public/dashboardGrafico.js
+++ public/dashboardGrafico.js
@@ -1,21 +1,38 @@
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart', 'line']});
         google.charts.setOnLoadCallback(drawChart);
 
         function drawChart() {
 
-            var data = google.visualization.arrayToDataTable([
-            ['Mês', 'Máquinas'],
-            ['Fevereiro', 2], ['Março', 5], ['Abril', 7], ['Maio', 10], ['Junho', 15]]);
+          var data = new google.visualization.DataTable();
+            data.addColumn('string', 'X');
+            data.addColumn('number', 'Máquinas');
+
+            data.addRows([['Fevereiro', 2], ['Março', 5], ['Abril', 7], ['Maio', 10], ['Junho', 15]]);
         
           var options = {
-            title: 'Tendência de perca de máquinas no decorrer dos meses',
-            hAxis: {title: 'Mês'},
-            vAxis: {title: 'Quantidade de máquinas'},
-            legend: 'none',
-            trendlines: { 0: {} } 
+            chart: {
+              title: 'Tendência de perca de máquinas (Proximos meses)',
+              subtitle: 'De acordo com os ultimos meses',
+              
+            },
+            hAxis: {
+              title: 'Mês',
+              titleTextStyle: { 
+                fontSize: '16',
+                bold: 'true',
+               }
+            },
+            vAxis: {
+              title: 'Quantidade de máquinas',
+              titleTextStyle: { 
+                fontSize: '16',
+                bold: 'true',
+               }
+            }
           };
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
+        var chart = new google.charts.Line(document.getElementById('linechart_material'));
+        
+        chart.draw(data, google.charts.Line.convertOptions(options));
       }

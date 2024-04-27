bb1ca7993be910200de4ea7587a1a7948b195971
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2024-04-27 17:10:19 -0300

    refactor: Alteração em query sql

--- public/cadastroPessoal.js
+++ public/cadastroPessoal.js
@@ -1,26 +1,19 @@
 function Continuar() {
-    
-    const selectCargo = cargo.value;
     const emailPessoal = emailPessoa.value;
     const loginSenha = senha.value;
     const nomeResp = nomeResponsavel.value;
-    const telResp = telPessoal.value;
     
     const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
-    if (emailPessoal.length <= 0 || loginSenha.length <= 0 || nomeResp.length <= 0 || telResp.length <= 0) {
+    if (emailPessoal.length <= 0 || loginSenha.length <= 0 || nomeResp.length <= 0) {
         alert("Preencha corretamente o formulário");
     } else if (!emailRegex.test(emailPessoal)) {
         alert("Insira um e-mail válido, contendo @, números e .com ");
-    } else if (telResp.length <= 10 || telResp.length >= 15) {
-        alert("Insira um número de telefone válido!");
     } else {
         const dadosPessoal = {
-            cargoPessoal: selectCargo,
+            nomePessoal: nomeResp,
             emailPessoal: emailPessoal,
             senhaPessoal: loginSenha,
-            nomePessoal: nomeResp,
-            telefonePessoal: telResp
         }
         cadastrarUsuario(dadosPessoal);
     }
--- src/infra/conexao.js
+++ src/infra/conexao.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const conexao = mysql.createConnection({
     host: "localhost",
-    database: "safe-learn",
+    database: "safelearn",
     user: "root",
-    password: "urubu100"
+    password: "senha"
 });
 
 module.exports = conexao;

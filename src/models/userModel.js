b9f3f24f55d4b6061dd5864351f4f0adc69798dc
daniescobarx <daniela.mollo@sptech.school>
2024-06-10 22:41:13 -0300

    ajuste model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,28 @@
 const db = require("../infra/conexao");
 
 function cadastrarUsuario(nome, email, senha, nivelAcesso) {
-    const instrucao = `INSERT INTO usuario (userName, emailUsuario, senhaUsuario, fkNivelDeAcesso) VALUES ('${nome}', '${email}', '${senha}', '${nivelAcesso}')`;
-    return db.executar(instrucao);
+    const instrucao = `INSERT INTO usuario (userName, emailUsuario, senhaUsuario, fkNivelDeAcesso) VALUES (?, ?, ?, ?)`;
+    return db.query(instrucao, [nome, email, senha, nivelAcesso]);
+}
+
+function obterUsuarios() {
+    const instrucao = `SELECT * FROM usuario`;
+    return db.query(instrucao);
+}
+
+function atualizarUsuario(id, nome, email, senha, nivelAcesso) {
+    const instrucao = `UPDATE usuario SET userName = ?, emailUsuario = ?, senhaUsuario = ?, fkNivelDeAcesso = ? WHERE idUsuario = ?`;
+    return db.query(instrucao, [nome, email, senha, nivelAcesso, id]);
+}
+
+function excluirUsuario(id) {
+    const instrucao = `DELETE FROM usuario WHERE idUsuario = ?`;
+    return db.query(instrucao, [id]);
 }
 
 module.exports = {
-    cadastrarUsuario
+    cadastrarUsuario,
+    obterUsuarios,
+    atualizarUsuario,
+    excluirUsuario
 };

734cbcfb9912e89517579f0d2868470fe94e70fe
daniescobarx <daniela.mollo@sptech.school>
2024-06-10 22:09:03 -0300

    feat: add criacao

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,28 +1,10 @@
 const db = require("../infra/conexao");
 
-const getAllUsers = async () => {
-  const [rows] = await db.query('SELECT * FROM usuario');
-  return rows;
-};
-
-const createUser = async (user) => {
-  const { userName, emailUsuario, senhaUsuario, fkNivelDeAcesso, fkInstituicao } = user;
-  const [result] = await db.query('INSERT INTO usuario (userName, emailUsuario, senhaUsuario, fkNivelDeAcesso, fkInstituicao) VALUES (?, ?, ?, ?, ?)', [userName, emailUsuario, senhaUsuario, fkNivelDeAcesso, fkInstituicao]);
-  return result.insertId;
-};
-
-const updateUser = async (id, user) => {
-  const { userName, emailUsuario, senhaUsuario, fkNivelDeAcesso } = user;
-  await db.query('UPDATE usuario SET userName = ?, emailUsuario = ?, senhaUsuario = ?, fkNivelDeAcesso = ? WHERE idUsuario = ?', [userName, emailUsuario, senhaUsuario, fkNivelDeAcesso, id]);
-};
-
-const deleteUser = async (id) => {
-  await db.query('DELETE FROM usuario WHERE idUsuario = ?', [id]);
-};
+function cadastrarUsuario(nome, email, senha, nivelAcesso) {
+    const instrucao = `INSERT INTO usuario (userName, emailUsuario, senhaUsuario, fkNivelDeAcesso) VALUES ('${nome}', '${email}', '${senha}', '${nivelAcesso}')`;
+    return db.executar(instrucao);
+}
 
 module.exports = {
-  getAllUsers,
-  createUser,
-  updateUser,
-  deleteUser
+    cadastrarUsuario
 };
